4dea8ebb2e41f8d3393d09d035320e8c17883ab2
chinklaus <chinklaus@gmail.com>
2021-08-23 19:07:15 +0800

    Feat: Nextjs Sanity template build up next-sanity config (lib folder) groq api function PortableText to parse richtext from santiy API

--- container/HomePage.js
+++ container/HomePage.js
@@ -1,11 +1,13 @@
 import HeadMeta from '@/components/HeadMeta';
+import { PortableText } from '@/lib/sanity';
 
-export default function HomePage() {
+export default function HomePage({ mainData }) {
+  const { title, content } = mainData;
   return (
     <>
-      <HeadMeta title="Klaus Chin" description="Klaus Chin Site" />
+      <HeadMeta title={title} description="Klaus Chin Site" />
       <div className="home-page f-h">
-        <h1>kk</h1>
+        <PortableText blocks={content} />
       </div>
       <style global jsx>{`
         .home-page {
