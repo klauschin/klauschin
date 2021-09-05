034f88aa03a4799be4e8655ffce5c4b2293f79a4
chinklaus <chinklaus@gmail.com>
2021-09-05 19:38:18 +0800

    Feat: fonts, homepage basic styles

--- container/HomePage.js
+++ container/HomePage.js
@@ -5,12 +5,18 @@ export default function HomePage({ mainData }) {
   const { title, content } = mainData;
   return (
     <>
-      <HeadMeta title={title} description="Klaus Chin Site" />
-      <div className="home-page f-h">
+      <HeadMeta title={title} description={content} />
+      <div className="home-page bg-offwhite f-h-center">
         <PortableText blocks={content} />
       </div>
       <style global jsx>{`
         .home-page {
+          min-height: 100vh;
+          width: 100%;
+          justify-content: center;
+          > div {
+            max-width: 900px;
+          }
         }
       `}</style>
     </>
