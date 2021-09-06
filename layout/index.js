d23bb75a5c4b741d175931db2b28b77d43a2818c
chinklaus <chinklaus@gmail.com>
2021-09-06 10:01:19 +0800

    Add: lerna.json, vercel.json

--- layout/index.js
+++ layout/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import Body from './Body';
+import Head from 'next/head';
+import Link from 'next/link';
 
 const Layout = (props) => {
-  return <Body>{props.children}</Body>;
+  return (
+    <>
+      <Head>
+        <Link
+          rel="preload"
+          href="/fonts/font-aw-conqueror-light.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <Link
+          rel="preload"
+          href="/fonts/font-aw-conqueror-light.woff"
+          as="font"
+          crossOrigin=""
+        />
+        <Link
+          rel="preload"
+          href="/fonts/font-aw-conqueror-light.woff2"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
+      <Body>{props.children}</Body>
+    </>
+  );
 };
 
 export default Layout;
